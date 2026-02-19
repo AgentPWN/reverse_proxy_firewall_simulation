7af55fbbdf6a7749567346d8e1c914bd50f442ed
AgentPWN <rupakbanerjee170@gmail.com>
2026-02-19 14:41:54 +0530

    dispatcher v1

--- index.go
+++ index.go
@@ -10,26 +10,21 @@ type worker struct{
 	value int
 	health chan bool
 }
-func (w* worker) job (){
-	
+
+type dispatcher struct{
+	value chan int
+
+}
+func (w* worker) job (){	
 	for healthy := range w.health{
 		if healthy{
-			fmt.Printf("%d\n",w.value)
+			fmt.Printf("worker %d: %d\n",w. id, w.value)
 		}
-		// fmt.Printf("%b\n",healthy)
-
-		
 	}
 }
-func main(){
-	var w worker
-	w.id = 1
-	w.health = make(chan bool)
-	w.value = 1
-	now := time.Now()
-	go w.job()
-	// fmt.Printf("hellow world")
-	for i:= 0 ;time.Since(now) < 30*time.Second; i++{
+
+func (w *worker)health_controller(){
+	for ;;{
 		if rand.Intn(3)<1{
 			w.health<-false
 		} else {
@@ -38,6 +33,38 @@ func main(){
 		time.Sleep(1*time.Second)
 		w.value++
 	}
-	// time.Sleep(10*time.Second)
-	close(w.health)
+}
+
+func (d *dispatcher) dispatch_controller(w_1 worker, w_2 worker){
+	value := <- d.value
+	fmt.Println("Dispatcher", value)
+
+}
+
+func main(){
+	var w_1 worker
+	w_1.id = 1
+	w_1.health = make(chan bool)
+	w_1.value = 1
+	go w_1.job()
+	go w_1.health_controller()
+	
+	var w_2 worker
+	w_2.id = 2
+	w_2.health = make(chan bool)
+	w_2.value = 1
+	go w_2.job()
+	go w_2.health_controller()
+	now := time.Now()
+	var d dispatcher
+	go d.dispatch_controller(w_1, w_2)
+	d.value = make(chan int)
+	for i:= 0 ;time.Since(now) < 5*time.Second; i++{
+		fmt.Println("timer:", i)
+		d.value <- i
+		time.Sleep(1*time.Second)
+	}
+	close(w_1.health)
+	close(w_2.health)
+
 }
