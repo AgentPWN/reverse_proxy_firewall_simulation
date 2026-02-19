2fc42b018b7288ea8c94a60cdb7777b136ad1b51
AgentPWN <rupakbanerjee170@gmail.com>
2026-02-19 14:05:56 +0530

    health checks v1

--- index.go
+++ index.go
@@ -5,32 +5,39 @@ import (
 	"math/rand"
 	// "sync"
 )
-type routine struct{
-	health bool
+type worker struct{
 	id int
+	value int
+	health chan bool
 }
+func (w* worker) job (){
+	
+	for healthy := range w.health{
+		if healthy{
+			fmt.Printf("%d\n",w.value)
+		}
+		// fmt.Printf("%b\n",healthy)
 
-func worker(id int, channel chan int){
-	for job := range channel{
-		fmt.Printf("%d,%d\n",id,job)
+		
 	}
 }
-func main() {
+func main(){
+	var w worker
+	w.id = 1
+	w.health = make(chan bool)
+	w.value = 1
 	now := time.Now()
-	var channel = make(chan int)
-	thread := routine{true, 0}
-	go worker(0, channel)
-	for i := 0; time.Since(now) < 10*time.Second; i++{
-
-		if rand.Intn(2)<1{
-			go worker(i,channel)
+	go w.job()
+	// fmt.Printf("hellow world")
+	for i:= 0 ;time.Since(now) < 30*time.Second; i++{
+		if rand.Intn(3)<1{
+			w.health<-false
+		} else {
+			w.health <- true
 		}
-		if rand.Intn(3)<2{
-			go worker(i,channel)
-		}
-		channel<-i
-
 		time.Sleep(1*time.Second)
+		w.value++
 	}
-	close(channel)
+	// time.Sleep(10*time.Second)
+	close(w.health)
 }
